72a6cc4e75e8386c22c6c7bd50b9be93fa763390
andrewmckishnie <59260061+andrewmckishnie@users.noreply.github.com>
2020-08-28 10:32:20 -0400

    Update newsletter_harvesting.py

--- newsletter_harvesting.py
+++ newsletter_harvesting.py
@@ -4,70 +4,11 @@ from email.header import decode_header
 import webbrowser
 import os
 import re
+import newsletter_utils
 import mysql.connector
 from mysql.connector import Error
-
-
-def capwords(words):
-    always_lower_words = ['a', 'an', 'and', 'the', 'or']
-    always_upper_words = ['BIPOC']
-    words_s = words.split()
-    init_word = words_s[0].upper() if words_s[0].upper() in always_upper_words else words_s[0].capitalize()
-    words_s.remove(words_s[0])
-    words_cap = [word.lower() if word.lower() in always_lower_words else word.upper() if word.upper() in always_upper_words else word.capitalize() for word in words_s]
-    non_init_words = ' '.join(words_cap)
-    processed_words = init_word + ' ' + non_init_words
-    return processed_words
     
-
-def process_subject(subject):
-    src_pattern = 'NL:MLFPC:(.*?):'
-    date_pattern = 'NL:MLFPC:.*?:(.*?):'
-    topic_1_pattern = 'NL:MLFPC:.*?:.*?:(.*?):'
-    topic_2_pattern = 'NL:MLFPC:.*?:.*?:.*?:(.*?):'
-    topic_3_pattern = 'NL:MLFPC:.*?:.*?:.*?:.*?:(.*?):'
-    topic_4_pattern = 'NL:MLFPC:.*?:.*?:.*?:.*?:.*?:(.*?):'
-    topic_5_pattern = 'NL:MLFPC:.*?:.*?:.*?:.*?:.*?:.*?:(.*?):'
-    source = re.findall(src_pattern, subject)
-    date_ = re.findall(date_pattern, subject)
-    topic_1 = re.findall(topic_1_pattern, subject)
-    topic_2 = re.findall(topic_2_pattern, subject)
-    topic_3 = re.findall(topic_3_pattern, subject)
-    topic_4 = re.findall(topic_4_pattern, subject)
-    topic_5 = re.findall(topic_5_pattern, subject)
-    src = capwords(source[0])
-    dat = capwords(date_[0])
-    top_1 = capwords(topic_1[0])
-    if topic_2:
-        top_2 = capwords(topic_2[0])
-        if topic_3:
-            top_3 = capwords(topic_3[0])
-            if topic_4:
-                top_4 = capwords(topic_4[0])
-                if topic_5:
-                    top_5 = capwords(topic_5[0])
-                    print("Topic 5: ", top_5)
-                else:
-                    top_5 = 'N/A'
-            else:
-                top_4 = 'N/A'
-                top_5 = 'N/A'
-        else:
-            top_3 = 'N/A'
-            top_4 = 'N/A'
-            top_5 = 'N/A'
-                    
-    else:
-        top_2 = 'N/A'
-        top_3 = 'N/A'
-        top_4 = 'N/A'
-        top_5 = 'N/A'
-    return src, dat, top_1, top_2, top_3, top_4, top_5
-                
-    
-    
-    
-def find_newsletters():
+def harvest_newsletters():
     
     username = ""
     password = ""
@@ -179,5 +120,5 @@ def find_newsletters():
             print("MySQL connection is closed")
 
 
-find_newsletters()
+harvest_newsletters()
 
